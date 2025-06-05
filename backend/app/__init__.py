ae260787b2b2f29512e4acc88bf053e7c49756fa
patco18 <hpatco18@gmail.com>
2025-06-05 12:22:25 +0000

    Ajout fichier static

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,12 +1,22 @@
-from flask import Flask
+from flask import Flask, send_from_directory
 from flask_sqlalchemy import SQLAlchemy
+import os
 
 db = SQLAlchemy()
 
 def create_app():
-    app = Flask(__name__)
+    app = Flask(__name__, static_folder='static', static_url_path='')
+
     app.config.from_object('config.Config')
     db.init_app(app)
-    from app.routes import hello
-    app.register_blueprint(hello.main)
+
+    # Enregistrement des blueprints
+    from app.routes.hello import main
+    app.register_blueprint(main)
+
+    # Route pour l'accueil (serveur frontend compilé)
+    @app.route('/')
+    def index():
+        return send_from_directory(app.static_folder, 'index.html')
+
     return app
