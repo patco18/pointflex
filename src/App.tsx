992199fc64040a5e748076bb6ac389a3f8e1297d
patco18 <157325021+patco18@users.noreply.github.com>
2025-08-24 21:47:33 +0000

    feat(router): enable v7 future flags

--- src/App.tsx
+++ src/App.tsx
@@ -135,7 +135,7 @@ function App() {
 
   return (
     <AuthProvider>
-      <Router>
+      <Router future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
         <Routes>
           <Route path="/login" element={<Login />} />
           
